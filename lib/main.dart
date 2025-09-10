0af6dbb054927d5fa97a48f3f8b7d266ba054812
wahabniazi09 <abdulwahab2301d@aptechgdn.net>
2025-09-10 11:11:52 +0500

    project

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:pet_care/consts/colors.dart';
+import 'package:pet_care/consts/consts.dart';
+import 'package:pet_care/firebase_options.dart';
+import 'package:pet_care/screen/splashScreen/splashScreen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const PetCareApp());
+}
+
+class PetCareApp extends StatelessWidget {
+  const PetCareApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Pet Care App",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: AppBarTheme(
+            iconTheme: IconThemeData(color: darkFontGrey),
+            backgroundColor: Colors.transparent),
+        fontFamily: regular,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
